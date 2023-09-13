abe32e5f629a4022a04576867b00e7f117a40822
Stepan Belenov <Sabaneev99@yandex.ru>
2023-09-13 15:39:09 +0300

    React - правки №2 - верстка

--- src/utils/Cards.js
+++ src/utils/Cards.js
@@ -29,6 +29,48 @@ const cards = [
     duration: "1ч 17м",
     link: "https://i116.fastpic.org/big/2022/0313/38/899a60e2fa7aea1076aceaa5b9605738.jpg",
   },
+  {
+    _id: 6,
+    name: "Книготорговцы",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/63/3f390b494892311bb6e20a3c0c269563.jpg",
+  },
+  {
+    _id: 7,
+    name: "Когда я думаю о Германии ночью",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/54/a4ef6f346f7f61b7328bd30107d17654.jpg",
+  },
+  {
+    _id: 8,
+    name: "Gimme Danger: История Игги и The Stooges",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/56/b31d262aa126fb82f3a4d01276912b56.jpg",
+  },
+  {
+    _id: 9,
+    name: "Дженис: Маленькая девочка грустит",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/8a/34bf367d0aa1e5a33fb8d705025e728a.jpg",
+  },
+  {
+    _id: 10,
+    name: "Соберись перед прыжком",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/81/868d814e74f198f710006d678b0e7981.jpg",
+  },
+  {
+    _id: 11,
+    name: "Пи Джей Харви: A dog called money",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/30/f9ddb06a263a8db4e272047a18ce1030.jpg",
+  },
+  {
+    _id: 12,
+    name: "По волнам: Искусство звука в кино",
+    duration: "1ч 17м",
+    link: "https://i116.fastpic.org/big/2022/0313/66/6322ced78f7d6298830b2aee708bb966.jpg",
+  },
 ];
 
 export default cards;
